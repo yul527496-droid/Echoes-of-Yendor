12195157322049590a9a7065022134671266f640
yul527496-droid <yul527496@gmail.com>
2026-08-14 00:09:33 +0800

    Refine ledger art and trigger roadside story

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/RoadFarmerTrigger.java
+++
@@ -1,2 +0,0 @@
-package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs;
-final class RoadFarmerTrigger { private RoadFarmerTrigger(){} static boolean shouldStart(int heroY){ return heroY < 28; } }
