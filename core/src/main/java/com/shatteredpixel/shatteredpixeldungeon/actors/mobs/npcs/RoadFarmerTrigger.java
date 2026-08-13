6a1ea52423a9218d393b12064875fac9ba4c089a
yul527496-droid <yul527496@gmail.com>
2026-08-14 00:02:52 +0800

    Add farmer approach trigger helper

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/RoadFarmerTrigger.java
@@ -0,0 +1,2 @@
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs;
+final class RoadFarmerTrigger { private RoadFarmerTrigger(){} static boolean shouldStart(int heroY){ return heroY < 28; } }
