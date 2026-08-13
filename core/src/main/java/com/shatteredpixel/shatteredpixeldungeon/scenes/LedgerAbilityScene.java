d2ce95a4c3e7100b2288a9fcc79d66fbcb4467e1
yul527496-droid <yul527496@gmail.com>
2026-08-13 22:14:09 +0800

    Add ability handoff

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/LedgerAbilityScene.java
@@ -0,0 +1,5 @@
+package com.shatteredpixel.shatteredpixeldungeon.scenes;
+import com.watabou.noosa.Game;
+public class LedgerAbilityScene extends PixelScene{
+ @Override public void create(){super.create();Game.switchScene(LedgerTalentScene.class);}
+}
