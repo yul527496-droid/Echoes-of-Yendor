c260a5414c8f23c9ff8b03d767dfd14b6194434f
yul527496-droid <yul527496@gmail.com>
2026-08-15 04:03:41 +0800

    RPG: advance ruleset for locked T1 choices

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ReturningHeroBuildRules.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ReturningHeroBuildRules.java
@@ -9,7 +9,7 @@ package com.shatteredpixel.shatteredpixeldungeon;
 public final class ReturningHeroBuildRules {
 
     /** Increment whenever stored presets need to be revalidated. */
-    public static final int RULESET_VERSION = 4;
+    public static final int RULESET_VERSION = 5;
 
     /** Shared budget for ordinary equipment reconstruction. */
     public static final int EQUIPMENT_RECONSTRUCTION_BUDGET = 15;
