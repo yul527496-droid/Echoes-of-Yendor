36ad43c3260c249d2c31bb5904536d1d2177af9a
yul527496-droid <yul527496@gmail.com>
2026-08-14 12:19:23 +0800

    Apply unified ledger typography to return page

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/LedgerReturnScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/LedgerReturnScene.java
@@ -168,9 +168,6 @@ public class LedgerReturnScene extends PixelScene {
     }
 
     private RenderedTextBlock t(String value, int size, int color, int width) {
-        RenderedTextBlock block = PixelScene.renderTextBlock(value, size);
-        block.maxWidth(width);
-        block.hardlight(color);
-        return block;
+        return LedgerUI.text(value, size, color, width);
     }
 }
