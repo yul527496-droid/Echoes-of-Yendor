c3a5cb9a2440ad1ab3b7560e684b498cff218d94
yul527496-droid <yul527496@gmail.com>
2026-08-16 12:50:12 +0800

    Visual foundation: add surface civilian sprite set

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SurfaceVillagerSprite.java
@@ -0,0 +1,28 @@
+/* Echoes of Yendor modifications Copyright (C) 2026 */
+package com.shatteredpixel.shatteredpixeldungeon.sprites;
+
+import com.shatteredpixel.shatteredpixeldungeon.Assets;
+import com.watabou.noosa.TextureFilm;
+import com.watabou.utils.Random;
+
+/** Four compact civilian silhouettes authored to sit beside SPD's 14x14 NPC scale. */
+public class SurfaceVillagerSprite extends MobSprite {
+
+    public SurfaceVillagerSprite() {
+        super();
+        texture(Assets.Sprites.SURFACE_VILLAGERS);
+        TextureFilm film = new TextureFilm(texture, 14, 14);
+        int first = Random.Int(4) * 2;
+
+        idle = new Animation(3, true);
+        idle.frames(film, first, first, first + 1, first, first);
+
+        run = new Animation(8, true);
+        run.frames(film, first, first + 1);
+
+        attack = idle.clone();
+        die = new Animation(20, false);
+        die.frames(film, first);
+        idle();
+    }
+}
