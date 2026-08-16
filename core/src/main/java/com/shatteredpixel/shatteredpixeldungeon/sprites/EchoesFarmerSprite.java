2731e0ae3ab9e2f033f4ec8c1ff78dad3038a5fb
yul527496-droid <yul527496@gmail.com>
2026-08-16 17:55:09 +0800

    0.0.9 art: switch farmer to production repaint

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/EchoesFarmerSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/EchoesFarmerSprite.java
@@ -2,9 +2,9 @@ package com.shatteredpixel.shatteredpixeldungeon.sprites;
 
 import com.watabou.noosa.TextureFilm;
 
-/** File-backed roadside farmer redrawn to match SPD's 16px map-sprite grammar. */
+/** File-backed roadside farmer repainted against SPD's compact 16px human-NPC scale. */
 public class EchoesFarmerSprite extends MobSprite {
-    private static final String TEXTURE = "sprites/echoes_farmer_v1.png";
+    private static final String TEXTURE = "sprites/echoes_farmer_v2.png";
 
     public EchoesFarmerSprite() {
         super();
