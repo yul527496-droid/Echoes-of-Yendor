5ed9fbb875b42c2ca07faa73ce6e8c70bd2784c0
yul527496-droid <yul527496@gmail.com>
2026-08-16 12:51:02 +0800

    Visual foundation: make civilian sprite atlas self-contained

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SurfaceVillagerSprite.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/sprites/SurfaceVillagerSprite.java
@@ -1,16 +1,17 @@
 /* Echoes of Yendor modifications Copyright (C) 2026 */
 package com.shatteredpixel.shatteredpixeldungeon.sprites;
 
-import com.shatteredpixel.shatteredpixeldungeon.Assets;
 import com.watabou.noosa.TextureFilm;
 import com.watabou.utils.Random;
 
 /** Four compact civilian silhouettes authored to sit beside SPD's 14x14 NPC scale. */
 public class SurfaceVillagerSprite extends MobSprite {
 
+    private static final String TEXTURE = "sprites/surface_villagers.png";
+
     public SurfaceVillagerSprite() {
         super();
-        texture(Assets.Sprites.SURFACE_VILLAGERS);
+        texture(TEXTURE);
         TextureFilm film = new TextureFilm(texture, 14, 14);
         int first = Random.Int(4) * 2;
 
