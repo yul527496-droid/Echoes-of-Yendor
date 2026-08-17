18b65e7300eb9aaf1bfaa549591431b2c1b6b2ed
yul527496-droid <yul527496@gmail.com>
2026-08-17 20:42:47 +0800

    feat: add Return donkey scene actor

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/npcs/ActOneReturnDonkey.java
@@ -0,0 +1,55 @@
+/* Echoes of Yendor modifications Copyright (C) 2026 */
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs.npcs;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.EchoesDonkeyCartSprite;
+
+/** Static, ordinary donkey/cart staging for the roadside wolf event. */
+public class ActOneReturnDonkey extends NPC {
+
+    {
+        spriteClass = EchoesDonkeyCartSprite.class;
+        HP = HT = 20;
+    }
+
+    @Override
+    protected boolean act() {
+        spend(TICK);
+        return true;
+    }
+
+    @Override
+    public boolean interact(Char c) {
+        return true;
+    }
+
+    @Override
+    public int defenseSkill(Char enemy) {
+        return INFINITE_EVASION;
+    }
+
+    @Override
+    public void damage(int dmg, Object src) {
+    }
+
+    @Override
+    public boolean add(Buff buff) {
+        return false;
+    }
+
+    @Override
+    public boolean reset() {
+        return true;
+    }
+
+    @Override
+    public String name() {
+        return "受惊的驴";
+    }
+
+    @Override
+    public String description() {
+        return "它紧贴着歪斜的木车，不安地蹬着地。狼群才是它眼下唯一关心的事。";
+    }
+}
