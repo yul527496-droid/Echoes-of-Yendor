e333f9c00faa89065104d97003040f886ed1b032
yul527496-droid <yul527496@gmail.com>
2026-08-17 20:43:00 +0800

    feat: add Return roadside wolves

---
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/mobs/ActOneReturnWolf.java
@@ -0,0 +1,43 @@
+/* Echoes of Yendor modifications Copyright (C) 2026 */
+package com.shatteredpixel.shatteredpixeldungeon.actors.mobs;
+
+import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
+import com.shatteredpixel.shatteredpixeldungeon.sprites.EchoesWolfSprite;
+import com.watabou.utils.Random;
+
+/** Low-stakes roadside wolf: ordinary scale after the Yog victory, not a boss. */
+public class ActOneReturnWolf extends Mob {
+
+    {
+        spriteClass = EchoesWolfSprite.class;
+        HP = HT = 12;
+        defenseSkill = 4;
+        EXP = 0;
+        maxLvl = 30;
+    }
+
+    @Override
+    public int damageRoll() {
+        return Random.NormalIntRange(2, 5);
+    }
+
+    @Override
+    public int attackSkill(Char target) {
+        return 9;
+    }
+
+    @Override
+    public int drRoll() {
+        return super.drRoll() + Random.NormalIntRange(0, 2);
+    }
+
+    @Override
+    public String name() {
+        return "灰狼";
+    }
+
+    @Override
+    public String description() {
+        return "一头被饥饿逼到旧路边的灰狼。和地下深处那些东西相比，它只是野兽。";
+    }
+}
